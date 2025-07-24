aeceb99796fc4ead023a1d004318bc2982801126
aubymori <aubyomori@gmail.com>
2025-07-24 00:56:32 -0500

    Compatibility with Windows 2000/Me and before

--- README.md
+++ README.md
@@ -6,6 +6,10 @@
 winver9x is a Win32 recreation of the `WINVER.EXE` applet from Windows 9x, the original version of which is
 a Win16 application and will not run on modern systems.
 
+## Building
+For most people, just building the winver9x project will be enough. However, if you want to run this on Windows
+2000/Me and before, you will need to build and run the pepatcher project on a compiled x86 winver9x.exe.
+
 ## Configuration
 winver9x can display different brandings. To do so, create the registry value
 `HKEY_CURRENT_USER\SOFTWARE\winver9x\Mode` as a DWORD, and set it to one of
---
+++ pepatcher/pepatcher.c
@@ -0,0 +1,99 @@
+#include <windows.h>
+#include <stdio.h>
+
+int wmain(int argc, wchar_t *argv[])
+{
+	int iRet = 0;
+
+	wprintf(L"winver9x PE Patcher\n");
+	wprintf(L"Because MSVC sucks\n\n");
+	
+	if (argc != 2)
+	{
+		wprintf(L"Usage: pepatcher <file>\n");
+		return 1;
+	}
+
+	HANDLE hFile = CreateFileW(argv[1], GENERIC_READ, NULL, NULL, OPEN_EXISTING, NULL, NULL);
+	if (!hFile || hFile == INVALID_HANDLE_VALUE)
+	{
+		wprintf(L"Failed to open file '%s'\n", argv[1]);
+		return GetLastError();
+	}
+
+	DWORD cbFile = GetFileSize(hFile, NULL);
+	LPBYTE lpFile = malloc(cbFile);
+	if (!lpFile)
+	{
+		wprintf(L"Failed to allocate buffer for file '%s'\n", argv[1]);
+		iRet = 1;
+		goto cleanup;
+	}
+
+	if (!ReadFile(hFile, lpFile, cbFile, NULL, NULL))
+	{
+		wprintf(L"Failed to read file '%s'", argv[1]);
+		iRet = GetLastError();
+		goto cleanup;
+	}
+
+	PIMAGE_DOS_HEADER pDosHead = (PIMAGE_DOS_HEADER)lpFile;
+	if (pDosHead->e_magic != 0x5A4D)
+	{
+		wprintf(L"Invalid DOS header\n");
+		iRet = 1;
+		goto cleanup;
+	}
+
+	PIMAGE_NT_HEADERS32 pNtHead = (PIMAGE_NT_HEADERS32)(lpFile + pDosHead->e_lfanew);
+	if (pNtHead->Signature != 0x00004550)
+	{
+		wprintf(L"Invalid NT header\n");
+		iRet = 1;
+		goto cleanup;
+	}
+
+	wprintf(L"Patching NT header for file '%s'\n", argv[1]);
+	
+	wprintf(
+		L"   Current OS version: %u.%u\n",
+		pNtHead->OptionalHeader.MajorOperatingSystemVersion,
+		pNtHead->OptionalHeader.MinorOperatingSystemVersion
+	);
+
+	wprintf(
+		L"   Current subsystem version: %u.%u\n",
+		pNtHead->OptionalHeader.MajorSubsystemVersion,
+		pNtHead->OptionalHeader.MinorSubsystemVersion
+	);
+
+	pNtHead->OptionalHeader.MajorOperatingSystemVersion = 4;
+	pNtHead->OptionalHeader.MinorOperatingSystemVersion = 0;
+	pNtHead->OptionalHeader.MajorSubsystemVersion       = 4;
+	pNtHead->OptionalHeader.MinorSubsystemVersion       = 0;
+
+	CloseHandle(hFile);
+	hFile = CreateFileW(argv[1], GENERIC_WRITE, NULL, NULL, OPEN_EXISTING, NULL, NULL);
+
+	if (!WriteFile(hFile, lpFile, cbFile, NULL, NULL))
+	{
+		wprintf(L"Failed to write to file '%s'\n", argv[1]);
+		iRet = GetLastError();
+		goto cleanup;
+	}
+	else
+	{
+		wprintf(L"Successfully patched\n");
+		iRet = GetLastError();
+		goto cleanup;
+	}
+
+
+cleanup:
+	if (hFile)
+		CloseHandle(hFile);
+	if (lpFile)
+		free(lpFile);
+
+	return iRet;
+}
